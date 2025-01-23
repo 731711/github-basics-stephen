0047e8e3c6bc4f7a22e3933d87bd2a5af22456ef
731711 <731711@hazelwoodschools.org>
2025-01-23 07:33:21 -0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+message >= 1
+while message = 1:
+	print("Hello World")
+	message = message + 1
+
+#message = message + 1 stops the loop after it increments
+#message variable is to use for the loop
